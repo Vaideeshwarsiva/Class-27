b7a0b934ddf5f55c2feb09b21ed0f46c2ce2a5b8
Vaideeshwarsiva <69429249+Vaideeshwarsiva@users.noreply.github.com>
2020-09-09 20:41:20 -0500

    Update chain.js

--- chain.js
+++ chain.js
@@ -6,12 +6,12 @@ class Chain {
             stiffness: 0.04,
             length: 10
         }
-        this.Chain = Constraint.create(options);
+        this.chain = Constraint.create(options);
         World.add(world, this.chain);
     }
     display() {
-        var pointA = this.Chain.bodyA.position;
-        var pointB = this.Chain.bodyB.position;
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.chain.bodyB.position;
         strokeWeight(3);
         line(pointA.x, pointA.y, pointB.x, pointB.y);
     }
